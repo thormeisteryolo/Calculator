022fd5c37d0e4caaba804fcb083bf910cee03c28
thor.jonsson <thor.jonsson@1H85172GSR>
2026-03-09 13:05:32 +0100

    Add project files.

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButtons(object sender, EventArgs e)
+        { 
+           Button button = (Button)sender;
+
+           EntryResult.Text += button.Text;
+        }
+
+        private void OperatorButtons(object sender, EventArgs e) 
+        {
+        Button button = ( Button )sender; 
+
+        EntryCalculations.Text += button.Text;
+        }
+    }
+}
